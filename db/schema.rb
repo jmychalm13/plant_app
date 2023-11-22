6bd2c7a0e3eced365811f3f3d522ef4ab0137a24
“Jessica <jmychalm13@gmail.com>
2023-11-22 12:43:15 -0600

    changed users table to include password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_122324) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_183839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,10 +57,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_122324) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
-    t.string "password_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "watering_schedules", force: :cascade do |t|
