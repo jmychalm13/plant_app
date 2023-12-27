61dbdae7854bdd272a461e0747ba1b1432c2de00
“Jessica <jmychalm13@gmail.com>
2023-12-26 21:28:12 -0600

    changed fixtures and added setup to tests to resolve a lot of errors

--- test/controllers/user_plants_controller_test.rb
+++ test/controllers/user_plants_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class UserPlantsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    User.create(first_name: "John", last_name: "Smith", email: "john@test.com", password: "password", password_confirmation: "password")
+    post "http://localhost:3000/sessions.json", params: {email: "john@test.com", password: "password"}
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/user_plants.json"
     assert_response 200
@@ -11,7 +18,8 @@ class UserPlantsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "UserPlant.count", 1 do
-      post "/user_plants.json", params: {user_id: 1, plant_id: 1, zone_id: 1, type_id: 1, img_url: "test.jpg"}
+      post "/user_plants.json", params: {user_id: 1, plant_id: 1, zone_id: 1, type_id: 1, img_url: "test.jpg"},
+      headers: {"Authorization" => "Bearer #{@jwt}"}
       assert_response 200
     end
   end
--- test/models/user_plant_test.rb
+++ test/models/user_plant_test.rb
@@ -1,7 +1,5 @@
 require "test_helper"
 
 class UserPlantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
