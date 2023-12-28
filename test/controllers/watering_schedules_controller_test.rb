558070f1f17cf6f7c8f5cee28cf5b6974e7906b7
“Jessica <jmychalm13@gmail.com>
2023-12-27 18:40:15 -0600

    added watering_schedules index test and passing

--- test/controllers/watering_schedules_controller_test.rb
+++ test/controllers/watering_schedules_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class WateringSchedulesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/watering_schedules.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal WateringSchedule.count, data.length
+  end
 end
