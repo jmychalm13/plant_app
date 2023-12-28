d375a73368cc033d969b45210e60b99a016aba5c
“Jessica <jmychalm13@gmail.com>
2023-12-27 18:47:42 -0600

    watering schedule create test added and passing

--- test/controllers/watering_schedules_controller_test.rb
+++ test/controllers/watering_schedules_controller_test.rb
@@ -16,4 +16,13 @@ class WateringSchedulesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal ["user_plant_id", "schedule"], data.keys
   end
+
+  test "create" do
+    assert_difference "WateringSchedule.count", 1 do
+      post "/watering_schedules.json",
+      params: {user_plant_id: UserPlant.first.id, schedule: "test schedule"}
+
+      assert_response :success
+    end
+  end
 end
