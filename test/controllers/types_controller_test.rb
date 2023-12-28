fae7b615f59b709f4bad0b0496a8537b6e0242be
“Jessica <jmychalm13@gmail.com>
2023-12-27 19:01:25 -0600

    Type index test added and passing

--- test/controllers/types_controller_test.rb
+++ test/controllers/types_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class TypesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/types.json"
+    assert_response :success
+
+    data = JSON.parse(response.body)
+    assert_equal Type.count, data.length
+  end
 end
