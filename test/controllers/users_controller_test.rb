51da0e4412519fc1b4ded23bb1498d8cec05d3b2
“Jessica <jmychalm13@gmail.com>
2023-12-28 18:52:45 -0600

    added user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    assert_difference "User.count", 1 do
+      post "/users.json", params: {
+        first_name: "John",
+        last_name: "Smith",
+        email: "john@test.com",
+        password: "password"
+      }
+      assert_response 201
+    end
+  end
 end
