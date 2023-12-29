fef1db006cb1ef65b77e73ef52b101ba6ef4c0b6
“Jessica <jmychalm13@gmail.com>
2023-12-28 19:09:54 -0600

    fertilizerSchedules controller completely passing all tests

--- test/controllers/fertilizer_schedules_controller_test.rb
+++ test/controllers/fertilizer_schedules_controller_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class FertilizerSchedulesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/fertilizer_schedules.json"
+    assert_response :success
+
+    data = JSON.parse(response.body)
+    assert_equal FertilizerSchedule.count, data.length
+  end
+
+  test "show" do
+    get "/fertilizer_schedules/#{FertilizerSchedule.first.id}.json"
+    assert_response :success
+
+    data = JSON.parse(response.body)
+    assert_equal ["user_plant_id", "schedule"], data.keys
+  end
+
+  test "create" do
+    assert_difference "FertilizerSchedule.count", 1 do
+      post "/fertilizer_schedules.json", params: {
+        user_plant_id: UserPlant.first.id,
+        schedule: "This is a test schedule."
+      }
+      assert_response :success
+    end
+  end
+
+  test "update" do
+    fertilizer_schedule = FertilizerSchedule.first
+    patch "/fertilizer_schedules/#{fertilizer_schedule.id}.json", params: {
+      schedule: "This is an updated test schedule"
+    }
+    assert_response :success
+  end
+
+  test "destroy" do
+    assert_difference "FertilizerSchedule.count", -1 do
+      delete "/fertilizer_schedules/#{FertilizerSchedule.first.id}.json"
+      assert_response :success
+    end
+  end
 end
