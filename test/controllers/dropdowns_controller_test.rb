302056c2ed8711d5cd0b4b291ae27a5e365044bf
“Jessica <jmychalm13@gmail.com>
2023-12-28 18:47:06 -0600

    added all tests for dropdown controller and passing

--- test/controllers/dropdowns_controller_test.rb
+++ test/controllers/dropdowns_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class DropdownsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "type" do
+    get "/dropdowns/type.json"
+    assert_response :success
+
+    data = JSON.parse(response.body)
+    assert_equal Type.count, data.length
+  end
+
+  test "zone" do
+    get "/dropdowns/zone.json"
+    assert_response :success
+
+    data = JSON.parse(response.body)
+    assert_equal Zone.count, data.length
+  end
+
+  test "plant" do
+    get "/dropdowns/plant.json"
+    assert_response :success
+
+    data = JSON.parse(response.body)
+    assert_equal UserPlant.count, data.length
+  end
 end
